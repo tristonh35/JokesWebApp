d92c0e3e34a6bb59888a2e87832846a770cae1cf
tristonh35 <tristonhall35@yahoo.com>
2022-01-20 15:09:46 -0800

    Add project files.

---
+++ Models/Joke.cs
@@ -0,0 +1,27 @@
+﻿namespace JokesWebApp.Models
+{
+    public class Joke
+    {
+        /// <summary>
+        /// The ID of a joke
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// The question of the joke
+        /// </summary>
+        public string JokeQuestion { get; set; }
+
+        /// <summary>
+        /// The answer to a specific joke
+        /// </summary>
+        public string JokeAnswer { get; set; }
+
+        public Joke()
+        {
+
+        }
+
+
+    }
+}
